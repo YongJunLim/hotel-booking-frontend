5bb77980f1b1e85681a1aa4c6a5f1816535e22b1
Poilik <tanchanglongstanley@gmail.com>
2025-07-20 14:45:25 +0800

    Updated Datepicker Styling and React-hook-form for all inputs

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,87 +1,133 @@
 import { Link } from 'wouter'
 // import useAuthStore from '../store'
-import { useState } from 'react'
+import { useEffect } from 'react'
 import DropdownWithButtons from '../DropDown'
-import DatePicker from 'react-datepicker'
-import 'react-datepicker/dist/react-datepicker.css'
 import { MyAccountDropdown } from './MyAccount'
-import type { Destination } from '../types/destination'
+import { destinationTypes, type Destination } from '../types/destination'
 import { TypeaheadSearch } from '../components/ui/TypeaheadSearch'
+import DayPicker from '../DayPicker'
+import { type Country, useRangeStore, useAdultStore, useChildStore, useRoomStore, useCountryStore } from '../States'
+import { useForm, type SubmitHandler  } from 'react-hook-form';
+import { useLocation } from 'wouter';
 
-function ReactDatePicker() {
-  const [startDate, setStartDate] = useState<Date | null>(null)
-  const [endDate, setEndDate] = useState<Date | null>(null)
-  const handleStartDateChange = (date: Date | null) => {
-    setStartDate(date)
-  }
-  const handleEndDateChange = (date: Date | null) => {
-    setEndDate(date)
-  }
-  const handleSubmit = () => {
-    if (startDate && endDate) {
-      alert(
-        `Selected range: ${startDate.toDateString()} - ${endDate.toDateString()}`,
-      )
-    }
-  }
-  const isBothSelected = startDate !== null && endDate !== null
-  return (
-    <div className="flex gap-4">
-      <div className="flex justify-center">
-        <DatePicker
-          placeholderText="Start Date"
-          selected={startDate}
-          onChange={handleStartDateChange}
-          dateFormat="dd/MM/yyyy"
-          className="text-center"
-          startDate={startDate}
-          endDate={endDate}
-          selectsStart
-          popperPlacement="bottom-start"
-        />
-      </div>
-      <p> - </p>
-      <div className="flex justify-center">
-        <DatePicker
-          placeholderText="End Date"
-          selected={endDate}
-          onChange={handleEndDateChange}
-          dateFormat="dd/MM/yyyy"
-          className="text-center"
-          startDate={startDate}
-          endDate={endDate}
-          selectsEnd
-          popperPlacement="bottom-start"
-        />
-      </div>
-
-      <button onClick={handleSubmit} disabled={!isBothSelected}>
-        Apply
-      </button>
-    </div>
-  )
-}
 
 export const HomePage = () => {
   // const { logout } = useAuthStore()
+  const { range, } = useRangeStore();
+  const { Adult, } = useAdultStore();
+  const { Child, } = useChildStore();
+  const { Room, } = useRoomStore();
+  const { country, setCountry } = useCountryStore();
+
+  const from  = range?.from;
+  const to = range?.to;
+  const start = from?.toISOString().split("T")[0];
+  const end = to?.toISOString().split("T")[0];
+  const sum = Adult + Child;
+
   const handleDestinationSelect = (destination: Destination) => {
+    setCountry(destination.uid, destination.term, destination.lat, destination.lng)
     console.log('Selected destination:', destination)
   }
+
+  type FormValues = {
+    start_?: string;
+    end_?: string;
+    sum_?: number;
+    Room_?: number;
+    country_?: Country;
+  }; 
+
+  const [, navigate] = useLocation();
+
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    setError,
+    clearErrors,
+    formState: { errors },
+  } = useForm<FormValues>({
+    defaultValues: {
+      start_: start,
+      end_: end,
+      sum_: sum,
+      Room_: Room,
+      country_: country
+    },
+    mode: "onSubmit",
+  });
+
+  useEffect(() => {
+    setValue('start_', start);
+    setValue('end_', end);
+    setValue('sum_', sum);
+    setValue('Room_', Room);
+    setValue('country_', country)
+  }, [start, end, sum, Room]);
+
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    console.log(data.country_);
+    if(data.start_ == undefined) {
+      setError('start_', {
+        type: 'manual',
+        message: 'Please select a start date.',
+      });
+      return;
+    } else {
+      clearErrors('start_' as any);
+    }
+
+    if(data.end_ == undefined) {
+      setError('end_', {
+        type: 'manual',
+        message: 'Please select an end date.',
+      });
+      return;
+    } else {
+      clearErrors('end_' as any);
+    }
+
+    if(data.sum_ == 0) {
+      setError('sum_', {
+        type: 'manual',
+        message: 'Please enter the number of guests.',
+      });
+      return;
+    } else {
+      clearErrors('sum_' as any);
+    }
+
+    if(data.Room_ == 0) {
+      setError('Room_', {
+        type: 'manual',
+        message: 'Please enter the number of rooms you require.',
+      });
+      return;
+    }
+    else {
+      clearErrors('Room_' as any);
+    }
+
+    if(data.country_?.uid == "") {
+      setError('country_', {
+        type: 'manual',
+        message: 'Please enter a destination.',
+      });
+      return;
+    } else {
+      clearErrors('country_' as any);
+    }
+
+    navigate(`/results/${ country.uid }?checkin=${ start }&checkout=${ end }&lang=en_US&currency=SGD&country_code=${ country.term }&guests=${ sum }|${ Room }`);
+  };
+  
   return (
     <>
       <div>
         <img className="h-96 w-full object-cover" src="/travel.jpg" />
       </div>
-      {/* <div
-        style={{
-          display: "flex",
-          gap: "20px",
-          alignItems: "center",
-          justifyContent: "space-between",
-        }}
-      > */}
       <div className="flex py-4 items-center justify-between gap-8">
-        {/* <div style={{ display: "flex", alignItems: "center", gap: "8px" }}> */}
         <div className="flex items-center gap-2">
           <h1 className="text-4xl font-bold mb-4">Hotel Booking</h1>
         </div>
@@ -92,40 +138,39 @@ export const HomePage = () => {
       <div>
         <p className="mb-4">Welcome to our hotel booking platform!</p>
       </div>
-      <div>
+      <div className="border border-gray-300 w-fit flex p-2">
         <DropdownWithButtons></DropdownWithButtons>
       </div>
       <div className="flex gap-8 py-4 width-100vw">
-        {/* <div style={{ width: "50vw" }}>
-          <MyComboBox>
-            {dest.map((dest) => (
-              <MyItem key={dest.term} id={dest.term}>
-                {dest.term}
-              </MyItem>
-            ))}
-          </MyComboBox>
-        </div> */}
         <TypeaheadSearch
-          onSelect={handleDestinationSelect}
-          placeholder="Search destinations..."
-          className="w-96"
-          limit={5}
-          threshold={0.3}
+        onSelect={handleDestinationSelect}
+        placeholder="Search destinations..."
+        className="w-250"
+        limit={5}
+        threshold={0.3}
         />
-        <div>
-          <div style={{ border: '1px solid #ccc' }} className="p-2 rounded">
-            <ReactDatePicker></ReactDatePicker>
-          </div>
+        <div className="p-2 rounded border border-gray-300 width-30vw">
+          <DayPicker></DayPicker>
         </div>
       </div>
       <div>
-        {/* May be preferable to use wouter's navigate for the actual search component */}
-        <Link
-          href="/results/WD0M?checkin=2025-10-01&checkout=2025-10-07&lang=en_US&currency=SGD&country_code=SG&guests=2|2"
-          className="btn btn-primary"
-        >
-          Search Hotels in WD0M
-        </Link>
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+          <div>
+            <input type="hidden" {...register("start_", { required: "Please select a start date." })} />
+            <input type="hidden" {...register("end_", { required: "Please select an end date." })} />
+            <input type="hidden" {...register("sum_", { required: "Please select the number of guests." })} />
+            <input type="hidden" {...register("Room_", { required: "Please select the number of rooms you require." })} />
+            <input type="hidden" {...register("Room_", { required: "Please enter a destination." })} />
+          </div>
+          {errors.start_ && <p className="text-red-500">{errors.start_.message}</p>}
+          {errors.end_ && <p className="text-red-500">{errors.end_.message}</p>}
+          {errors.sum_ && <p className="text-red-500">{errors.sum_.message}</p>}
+          {errors.Room_ && <p className="text-red-500">{errors.Room_.message}</p>}
+          {errors.country_ && <p className="text-red-500">{errors.country_.message}</p>}
+          <button type="submit" className="bg-blue-500 text-white px-3 py-1 rounded">
+            Search
+          </button>
+        </form>
       </div>
     </>
   )
